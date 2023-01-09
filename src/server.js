767a85b9c7a9bb2efe4435df160af800d9497d06
yaelBrown <yaelrbrown@gmail.com>
2023-01-08 20:36:25 -0600

    add: added fastify-cors

--- src/server.js
+++ src/server.js
@@ -2,6 +2,21 @@ const fastify = require('fastify')({
   logger: true
 })
 
+fastify.register(require('@fastify/cors'), (instance) => {
+  return (req, callback) => {
+    const corsOptions = {
+      // This is NOT recommended for production as it enables reflection exploits
+      origin: true
+    };
+    // do not include CORS headers for requests from localhost
+    if (/^localhost$/m.test(req.headers.origin)) {
+      corsOptions.origin = false
+    }
+    // callback expects two parameters: error and options
+    callback(null, corsOptions)
+  }
+})
+
 const PORT = 3001
 const API_VER = 'v1'
 

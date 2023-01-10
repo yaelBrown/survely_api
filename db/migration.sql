1e21b5a97c82af7f5c8524b826ff7ea387e1da92
yaelBrown <yaelrbrown@gmail.com>
2023-01-09 20:12:28 -0600

    feat: added constants to migration.sql

--- db/migration.sql
+++ db/migration.sql
@@ -1,44 +1,43 @@
 CREATE TABLE `users` (
-  `id` int PRIMARY KEY,
-  `email` varchar(255),
+  `id` int unsigned PRIMARY KEY,
+  `email` varchar(255) UNIQUE NOT NULL,
   `name` varchar(255),
-  `password` varchar(255),
+  `password` varchar(255) NOT NULL,
   `last_login` datetime,
-  `created_date` datetime
 );
 
 CREATE TABLE `users_meta` (
   `id` int PRIMARY KEY,
-  `user_id` int,
+  `user_id` int NOT NULL UNIQUE,
   `last_login` datetime,
   `created_date` datetime,
-  `service_level` enum,
-  `surveys_created_count` int,
-  `groups_created_count` int
+  `service_level` enum NOT NULl, -- set default service_level here 
+  `surveys_created_count` int DEFAULT 0,
+  `groups_created_count` int DEFAULT 0
 );
 
 CREATE TABLE `users_groups` (
   `id` int PRIMARY KEY,
-  `user_id` int,
-  `groups_id` int,
-  `can_read` boolean,
-  `can_create` boolean,
-  `can_delete` boolean,
-  `can_edit` boolean,
-  `can_add_people` boolean,
-  `can_rem_people` boolean,
-  `can_edit_people` boolean
+  `user_id` int NOT NULL,
+  `groups_id` int NOT NULL,
+  `can_read` boolean NOT NULL,
+  `can_create` boolean NOT NULL,
+  `can_delete` boolean NOT NULL,
+  `can_edit` boolean NOT NULL,
+  `can_add_people` boolean NOT NULL,
+  `can_rem_people` boolean NOT NULL,
+  `can_edit_people` boolean NOT NULL
 );
 
 CREATE TABLE `groups` (
   `id` int PRIMARY KEY,
-  `name` varchar(255),
-  `admin_id` int
+  `name` varchar(255) NOT NULL,
+  `admin_id` int NOT NULL
 );
 
 CREATE TABLE `groups_meta` (
   `id` int PRIMARY KEY,
-  `groups_id` int,
+  `groups_id` int NOT NULL,
   `created_date` datetime
 );
 
@@ -46,9 +45,9 @@ CREATE TABLE `surveys` (
   `id` int PRIMARY KEY,
   `surveyor_user_id` int,
   `surveyor_group_id` int,
-  `survey_name` varchar(255),
+  `survey_name` varchar(255) NOT NULL,
   `survey_date` datetime,
-  `survey_is_active` boolean,
+  `survey_is_active` boolean DEFAULT false,
   `surveyee_text` json,
   `surveyee_whatsapp` json,
   `surveyee_email` json,
@@ -57,16 +56,16 @@ CREATE TABLE `surveys` (
 
 CREATE TABLE `survey_responses` (
   `id` int PRIMARY KEY,
-  `survey_id` int,
+  `survey_id` int NOT NULL,
   `surveyee_id` varchar(255),
-  `question_id` varchar(255),
-  `response` varchar(255),
+  `question_id` varchar(255) NOT NULL,
+  `response` varchar(255) NOT NULL,
   `responded_date` datetime
 );
 
 CREATE TABLE `surveyee_list` (
   `id` int PRIMARY KEY,
-  `surveyor_user_id` int,
+  `surveyor_user_id` int NOT NULL,
   `surveyor_group_id` int,
   `surveyee_text` json,
   `surveyee_whatsapp` json,

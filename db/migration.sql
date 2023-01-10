10c0b8071508f1a6b9d0185a694c82a0413e28cf
yaelBrown <yaelrbrown@gmail.com>
2023-01-09 21:13:54 -0600

    fix: migration.sql

--- db/migration.sql
+++ db/migration.sql
@@ -1,25 +1,29 @@
+drop database if exists survely;
+create database survely; 
+use survely;
+
 CREATE TABLE `users` (
   `id` int unsigned PRIMARY KEY,
   `email` varchar(255) UNIQUE NOT NULL,
   `name` varchar(255),
   `password` varchar(255) NOT NULL,
-  `last_login` datetime,
+  `last_login` datetime
 );
 
 CREATE TABLE `users_meta` (
   `id` int PRIMARY KEY,
-  `user_id` int NOT NULL UNIQUE,
+  `user_id` int unsigned NOT NULL UNIQUE,
   `last_login` datetime,
   `created_date` datetime,
-  `service_level` enum NOT NULl, -- set default service_level here 
+  `service_level` enum('zero', 'one', 'two', 'admin') NOT NULL,
   `surveys_created_count` int DEFAULT 0,
   `groups_created_count` int DEFAULT 0
 );
 
 CREATE TABLE `users_groups` (
   `id` int PRIMARY KEY,
-  `user_id` int NOT NULL,
-  `groups_id` int NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  `groups_id` int unsigned NOT NULL,
   `can_read` boolean NOT NULL,
   `can_create` boolean NOT NULL,
   `can_delete` boolean NOT NULL,
@@ -30,21 +34,21 @@ CREATE TABLE `users_groups` (
 );
 
 CREATE TABLE `groups` (
-  `id` int PRIMARY KEY,
+  `id` int unsigned PRIMARY KEY,
   `name` varchar(255) NOT NULL,
   `admin_id` int NOT NULL
 );
 
 CREATE TABLE `groups_meta` (
-  `id` int PRIMARY KEY,
-  `groups_id` int NOT NULL,
+  `id` int unsigned PRIMARY KEY,
+  `groups_id` int unsigned NOT NULL,
   `created_date` datetime
 );
 
 CREATE TABLE `surveys` (
-  `id` int PRIMARY KEY,
-  `surveyor_user_id` int,
-  `surveyor_group_id` int,
+  `id` int unsigned PRIMARY KEY,
+  `surveyor_user_id` int unsigned,
+  `surveyor_group_id` int unsigned,
   `survey_name` varchar(255) NOT NULL,
   `survey_date` datetime,
   `survey_is_active` boolean DEFAULT false,
@@ -56,7 +60,7 @@ CREATE TABLE `surveys` (
 
 CREATE TABLE `survey_responses` (
   `id` int PRIMARY KEY,
-  `survey_id` int NOT NULL,
+  `survey_id` int unsigned NOT NULL,
   `surveyee_id` varchar(255),
   `question_id` varchar(255) NOT NULL,
   `response` varchar(255) NOT NULL,
@@ -65,8 +69,8 @@ CREATE TABLE `survey_responses` (
 
 CREATE TABLE `surveyee_list` (
   `id` int PRIMARY KEY,
-  `surveyor_user_id` int NOT NULL,
-  `surveyor_group_id` int,
+  `surveyor_user_id` int unsigned NOT NULL,
+  `surveyor_group_id` int unsigned,
   `surveyee_text` json,
   `surveyee_whatsapp` json,
   `surveyee_email` json
@@ -78,7 +82,7 @@ ALTER TABLE `users_meta` ADD FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 ALTER TABLE `surveyee_list` ADD FOREIGN KEY (`surveyor_user_id`) REFERENCES `users` (`id`);
 
-ALTER TABLE `groups` ADD FOREIGN KEY (`id`) REFERENCES `users_groups` (`groups_id`);
+ALTER TABLE `users_groups` ADD FOREIGN KEY (`groups_id`) REFERENCES `groups` (`id`);
 
 ALTER TABLE `surveys` ADD FOREIGN KEY (`surveyor_user_id`) REFERENCES `users` (`id`);
 
